0ee079f98feffdb0580288141dff27ec7b9e4991
Sergey Dobrikov <sergey.dobrikov@waveaccess.global>
2024-07-18 18:18:43 +0400

    Test formatter

--- pool/pool/doctype/poolemployeelink/poolemployeelink.js
+++ pool/pool/doctype/poolemployeelink/poolemployeelink.js
@@ -6,3 +6,23 @@
 
 // 	},
 // });
+
+//frappe.ui.form.on('PoolEmployeeLink', {
+//  onload: function (frm) {
+//    console.log("onload formatter test");
+//    frappe.form.link_formatters['My Doctype I want to Display'] = function (value, doc) {
+//      console.log("onload formatter for my doctype value=" + value + " doc.name=" + doc.name);
+//      return value + ":" + doc.name;
+//    }
+//  }
+//});
+
+frappe.form.link_formatters['Employee'] = function(value, doc) {
+    console.log("Link formatter test - " + doc.employee_name + " value - " + value);
+    console.log(doc);
+    if(doc.employee_name && doc.employee_name !== value) {
+        return value + ': ' + doc.employee_name;
+    } else {
+        return value;
+    }
+}
