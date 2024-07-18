5387d5f19d78b22027caa1f2979999f2d1b3a657
Sergey Dobrikov <sergey.dobrikov@waveaccess.global>
2024-07-18 16:15:54 +0400

    Add doctype for link Pool and Employee

---
+++ pool/pool/doctype/poolemployeelink/poolemployeelink.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Sergey Dobrikov and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PoolEmployeeLink(Document):
+	pass
